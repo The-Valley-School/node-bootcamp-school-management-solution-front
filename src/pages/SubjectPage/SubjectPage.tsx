d06996f8300fc0053e5cfd00ef91b0720b8a30bf
Fran Linde Blázquez <franlindebl@gmail.com>
2023-06-12 09:19:39 +0200

    Added router

---
+++ src/pages/SubjectPage/SubjectPage.tsx
@@ -0,0 +1,11 @@
+import "./SubjectPage.scss";
+
+const SubjectPage = (): JSX.Element => {
+  return (
+    <div className="subject-page">
+      <h1>SubjectPage!</h1>
+    </div>
+  );
+};
+
+export default SubjectPage;
---
+++ src/pages/UserPage/UserPage.tsx
@@ -0,0 +1,11 @@
+import "./UserPage.scss";
+
+const UserPage = (): JSX.Element => {
+  return (
+    <div className="user-page">
+      <h1>UserPage!</h1>
+    </div>
+  );
+};
+
+export default UserPage;
